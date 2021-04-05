100035f27a55f58e18acc725c52a415cb97acd4c
Iftekhar Mohammad <iftekhar@workspaceit.com>
2021-04-05 18:23:17 +0600

    Plugin Change

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import Tooltip from "../Blocks/Tooltip/Tooltip";
-import {Chart} from 'react-charts'
+import {Line} from 'react-chartjs-2';
 
 
 const Charts = ({chartData}) => {
 
     const processChartData = (chartData) => {
         let charts = chartData["histogram"] ? chartData["histogram"] : []
-        console.log(charts)
+        let plotPointTexts = []
         let plotPoints = []
-        charts.forEach( (val, index) => {
-            console.log(val)
-            plotPoints.push([index, val["count"]])
+        charts.forEach((val, index) => {
+            plotPointTexts.push(val["datetime"])
+            plotPoints.push(val["count"])
         })
-        return plotPoints
+
+        return {
+            labels: plotPointTexts,
+            datasets: [
+                {
+                    label: 'Log Occurrences',
+                    data: plotPoints,
+                    fill: false,
+                    pointBorderWidth: 3,
+                    pointHoverRadius: 5,
+                    borderColor: 'rgb(251, 191, 35)',
+                    pointBackgroundColor: '#827280',
+                },
+            ],
+        }
     }
 
-    const processedData = processChartData(chartData);
-    console.log(processedData)
 
-    const data = React.useMemo(
-        () => [
-            {
-                label: 'Log Chart',
-                data: processedData ? processedData : [[1,2],[2,4]]
-            }
-        ],
-        []
-    )
-
-    const axes = React.useMemo(
-        () => [
-            {primary: true, type: 'time', position: 'bottom'},
-            {type: 'linear', position: 'left'}
-        ],
-        []
-    )
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    }
     return (
         <div className="w-full lg:w-1/2 px-4">
             <div className="bg-white border-t border-b sm:rounded sm:border shadow">
@@ -54,10 +60,10 @@ const Charts = ({chartData}) => {
                 <div>
                     <div className="text-center px-6 py-4 overflow-hidden overflow-x-auto">
                         <div className="py-8" style={{
-                            width: '680px',
-                            height: '250px'
+                            minWidth: '680px',
+                            minHeight: '300px'
                         }}>
-                            <Chart data={data} axes={axes}/>
+                            <Line data={processChartData(chartData)} options={options}/>
                         </div>
                     </div>
                 </div>
